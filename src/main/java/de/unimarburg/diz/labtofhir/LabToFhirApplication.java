3a711a97a89a3ee060bde686c8e473ade4495af6
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2021-01-05 11:53:58 +0100

    Initial commit

---
+++ src/main/java/de/unimarburg/diz/labtofhir/LabToFhirApplication.java
@@ -0,0 +1,13 @@
+package de.unimarburg.diz.labtofhir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabToFhirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabToFhirApplication.class, args);
+	}
+
+}
