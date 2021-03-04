5283755fdac3cea8482fc6a0512a0d7866b33deb
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2021-03-04 17:44:54 +0100

    test: cleanup

--- src/test/java/de/unimarburg/diz/labtofhir/FhirObjectMapperTests.java
+++ src/test/java/de/unimarburg/diz/labtofhir/FhirObjectMapperTests.java
@@ -8,19 +8,16 @@ import java.io.IOException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 import org.springframework.kafka.support.JacksonUtils;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
 public class FhirObjectMapperTests {
 
+    private final ObjectMapper mapper = JacksonUtils.enhancedObjectMapper();
     @Value("classpath:test-input.json")
     Resource input;
-
-    private ObjectMapper mapper = JacksonUtils.enhancedObjectMapper();
 
     @Test
     public void inputMapsToModel() throws IOException {
