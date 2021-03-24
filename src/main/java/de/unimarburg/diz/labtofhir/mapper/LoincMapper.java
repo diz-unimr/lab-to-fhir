2de34a52e3a5601599feeed8ae7231b4a88171ba
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2021-03-24 13:28:45 +0100

    improve logging

--- src/main/java/de/unimarburg/diz/labtofhir/mapper/LoincMapper.java
+++ src/main/java/de/unimarburg/diz/labtofhir/mapper/LoincMapper.java
@@ -3,12 +3,15 @@ package de.unimarburg.diz.labtofhir.mapper;
 import de.unimarburg.diz.labtofhir.model.LoincMap;
 import javax.annotation.PostConstruct;
 import org.hl7.fhir.r4.model.Observation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoincMapper {
 
+    private final static Logger log = LoggerFactory.getLogger(LoincMapper.class);
     @Value("${mapping.loinc.file}")
     private String mappingFile;
     private LoincMap loincMap;
@@ -20,6 +23,11 @@ public class LoincMapper {
     public Observation mapCodeAndQuantity(Observation obs, String metaCode) {
         var coding = obs.getCode().getCoding().get(0);
         var entry = loincMap.get(coding.getCode(), metaCode);
+        if (entry == null) {
+            throw new IllegalArgumentException(String.format(
+                "LOINC mapping lookup failed. No values found for code: %s and meta code: %s",
+                coding.getCode(), metaCode));
+        }
 
         // map code
         coding.setCode(entry.getLoinc())
