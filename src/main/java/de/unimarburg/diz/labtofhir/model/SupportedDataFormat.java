58cb8238a0bef8ebfa10a1a52919a1237820ca61
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2024-09-11 11:34:28 +0200

    test: improve mapper test

--- src/main/java/de/unimarburg/diz/labtofhir/model/SupportedDataFormat.java
+++
@@ -1,8 +0,0 @@
-package de.unimarburg.diz.labtofhir.model;
-
-public final class SupportedDataFormat {
-
-    public static final String HL7 = "hl7";
-    public static final String AIM = "aim";
-
-}
--- src/test/java/de/unimarburg/diz/labtofhir/mapper/Hl7LabMapperTests.java
+++ src/test/java/de/unimarburg/diz/labtofhir/mapper/Hl7LabMapperTests.java
@@ -1,11 +1,13 @@
 package de.unimarburg.diz.labtofhir.mapper;
 
-import ca.uhn.hl7v2.DefaultHapiContext;
-import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.model.Message;
 import ca.uhn.hl7v2.model.v22.message.ORU_R01;
 import de.unimarburg.diz.labtofhir.configuration.FhirConfiguration;
+import de.unimarburg.diz.labtofhir.configuration.KafkaConfiguration;
+import org.apache.kafka.common.serialization.Serde;
 import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
+import org.hl7.fhir.r4.model.DiagnosticReport;
+import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.ServiceRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,42 +16,52 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.Resource;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(classes = {Hl7LabMapper.class, FhirConfiguration.class})
+@SpringBootTest(classes = {Hl7LabMapper.class, FhirConfiguration.class,
+    KafkaConfiguration.class})
 public class Hl7LabMapperTests {
 
-
+    @Autowired
+    private Serde<ORU_R01> hl7Serde;
     @Value("classpath:reports/test.hl7")
     private Resource testHl7;
-
     @Autowired
     private Hl7LabMapper mapper;
 
+    @SuppressWarnings("checkstyle:MagicNumber")
     @Test
-    public void messageIsMapped() throws IOException, HL7Exception {
+    public void messageIsMapped() throws IOException {
 
         var report = getTestReport(testHl7);
 
         var bundle = mapper.apply((ORU_R01) report);
 
-        var mapped =
+        var mappedReport =
             bundle.getEntry().stream().map(BundleEntryComponent::getResource)
-                .filter(ServiceRequest.class::isInstance)
-                .map(ServiceRequest.class::cast).findFirst().orElseThrow();
+                .filter(DiagnosticReport.class::isInstance)
+                .map(DiagnosticReport.class::cast).findFirst().orElseThrow();
 
-        assertThat(mapped.getId()).isEqualTo("20220702-88888888");
+        assertThat(mappedReport.getId()).isEqualTo("20190417-55555555");
+        // count resources
+        assertThat(
+            bundle.getEntry().stream().map(BundleEntryComponent::getResource)
+                .filter(ServiceRequest.class::isInstance).count()).isEqualTo(1);
+        assertThat(
+            bundle.getEntry().stream().map(BundleEntryComponent::getResource)
+                .filter(DiagnosticReport.class::isInstance).count()).isEqualTo(
+            1);
+        assertThat(
+            bundle.getEntry().stream().map(BundleEntryComponent::getResource)
+                .filter(Observation.class::isInstance).count()).isEqualTo(5);
     }
 
     private Message getTestReport(Resource testReport)
-        throws IOException, HL7Exception {
+        throws IOException {
 
-        try (var ctx = new DefaultHapiContext()) {
-            return ctx.getGenericParser()
-                .parse(testReport.getContentAsString(StandardCharsets.UTF_8));
-        }
+        return hl7Serde.deserializer()
+            .deserialize(null, testReport.getContentAsByteArray());
     }
 
 }
